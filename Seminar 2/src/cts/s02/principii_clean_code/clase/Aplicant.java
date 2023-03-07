5109905940f4c895a0a805855c981afb011f4e45
Ana Coman <comanana45@gmail.com>
2023-03-07 11:05:23 +0200

    Modificari SOLID

--- Seminar 2/src/cts/s02/principii_clean_code/clase/Aplicant.java
+++ Seminar 2/src/cts/s02/principii_clean_code/clase/Aplicant.java
@@ -7,62 +7,45 @@ public abstract class Aplicant{
 	protected String prenume;
 	protected int varsta;
 	protected int punctaj;
-	protected int nr_proiecte;
+	protected int numarProiecte;
 	protected String[] denumireProiect;
-	
-	
+
+	public Aplicant(String nume, String prenume, int varsta, int punctaj, int numarProiecte, String[] denumireProiect) {
+		super();
+		this.nume = nume;
+		this.prenume = prenume;
+		this.varsta = varsta;
+		this.punctaj = punctaj;
+		this.numarProiecte = numarProiecte;
+		this.denumireProiect = denumireProiect;
+	}
+
 	public String getNume() {
 		return nume;
 	}
-	public void setNume(String nume) {
-		this.nume = nume;
-	}
+
 	public String getPrenume() {
 		return prenume;
-	}
-	public void setPrenume(String prenume) {
-		this.prenume = prenume;
 	}
 	public int getVarsta() {
 		return varsta;
 	}
-	public void setVarsta(int varsta) {
-		this.varsta = varsta;
-	}
-	public void statut(){
-		if(punctaj>80)
-			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
-		else
-			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
-		}
 	public int getPunctaj() {
 		return punctaj;
 	}
-	public void setPunctaj(int punctaj) {
-		this.punctaj = punctaj;
-	}
-	
-	
 
-	
-	public Aplicant() {
-		super();
-		// TODO Auto-generated constructor stub
+	public int getNumarProiecte() {
+		return numarProiecte;
 	}
-	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect) {
-		super();
-		this.nume = nume;
-		this.prenume = prenume;
-		this.varsta = varsta;
-		this.punctaj = punctaj;
-		this.nr_proiecte = nr_proiecte;
-		this.denumireProiect = denumireProiect;
-	}
-	public int getNr_proiecte() {
-		return nr_proiecte;
-	}
-	public void setNr_proiecte(int nr_proiecte) {
-		this.nr_proiecte = nr_proiecte;
-	}
+
+	public void statut(){
+		if(punctaj>80)
+		{
+			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
+		}
+		else
+			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
+		}
+
 
 }
--- Seminar 2/src/cts/s02/principii_clean_code/main/Program.java
+++ Seminar 2/src/cts/s02/principii_clean_code/main/Program.java
@@ -3,20 +3,20 @@ package cts.s02.principii_clean_code.main;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import cts.s02.principii_clean_code.clase.Angajat;
-import cts.s02.principii_clean_code.clase.AngajatReader;
-import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.clase.*;
 import cts.s02.principii_clean_code.clase.readers.AplicantReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAngajati;
+
 		try {
 			AplicantReader aplicantReader=new AngajatReader();
 			listaAngajati = aplicantReader.readAplicants("angajati.txt");
 			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
