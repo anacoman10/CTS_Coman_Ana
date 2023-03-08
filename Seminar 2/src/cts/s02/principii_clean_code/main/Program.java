03cd6c419d058292f26dd332d2943e32cdf1f512
Ana Coman <comanana45@gmail.com>
2023-03-08 11:31:45 +0200

    Seminar 03 SOLID

--- Seminar 2/src/cts/s02/principii_clean_code/main/Program.java
+++ Seminar 2/src/cts/s02/principii_clean_code/main/Program.java
@@ -1,25 +1,28 @@
 package cts.s02.principii_clean_code.main;
 
+import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import cts.s02.principii_clean_code.readers.AplicantReader;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import cts.s02.principii_clean_code.clase.*;
-import cts.s02.principii_clean_code.clase.readers.AplicantReader;
 
 public class Program {
-
 	public static void main(String[] args) {
 		List<Aplicant> listaAngajati;
-
 		try {
-			AplicantReader aplicantReader=new AngajatReader();
+			AplicantReader aplicantReader = new AngajatReader();
 			listaAngajati = aplicantReader.readAplicants("angajati.txt");
-			for(Aplicant angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			for (Aplicant aplicant : listaAngajati) {
+				System.out.println(aplicant.toString());
+				aplicant.afisareFinantare();
+			}
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-	}
 
+
+	}
 }
